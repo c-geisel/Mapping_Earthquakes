a5390eaba9e0665507db6e65121b4ee7127e8339
Chloe Ittel <chloeittel@Chloes-Air.woh.rr.com>
2021-11-06 16:38:29 -0400

    updates

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,18 +1,12 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center and zoom level.
-//let map = L.map('mapid').setView([30, 30], 2);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 10,
     accessToken: API_KEY
 });
-
-// Then we add our 'graymap' tile layer to the map.
-//streets.addTo(map);
 
 // We create the dark view tile layer that will be an option for our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -37,7 +31,6 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/c-geisel/Mapping_Earthquakes/main/majorAirports.json";
 
@@ -54,4 +47,3 @@ d3.json(airportData).then(function(data) {
     }
   }).addTo(map);
 });
-
--- Mapping_GeoJSON_Points/static/js/config.js
+++
@@ -1,1 +0,0 @@
-const API_KEY = "pk.eyJ1IjoiYy1nZWlzZWwiLCJhIjoiY2t2bDY1cmR3MnE3bTJvcWZkd2RqNjBhMyJ9.3UhCa3YizYSUjbKzWdrchw"
